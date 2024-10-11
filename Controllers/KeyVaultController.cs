0ba83b01e4670fe942bdaffab71967b6c271783e
Marcos G. Júnior <markos-jr@outlook.com>
2024-10-11 00:17:42 -0300

    feat: Implementação inicial do KeyVaultController

---
+++ Controllers/KeyVaultController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KeyVaultPoC.Services;
+
+namespace KeyVaultPoC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KeyVaultController : ControllerBase
+    {
+        private readonly SecretService _secretService;
+
+        public KeyVaultController(SecretService secretService)
+        {
+            _secretService = secretService;
+        }
+
+        [HttpGet("get-credentials")]
+        public async Task<IActionResult> GetCredentials()
+        {
+            string user = await _secretService.GetSecretAsync("ApiUser");
+            string password = await _secretService.GetSecretAsync("ApiUser");
+
+            return Ok(new { User = user, Password = password });
+        }
+    }
+}

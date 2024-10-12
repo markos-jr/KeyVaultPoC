384c6b9998706d2c6a53416ff27a5a94f1d3f8e8
Marcos G. Júnior <markos-jr@outlook.com>
2024-10-12 18:45:07 -0300

    feat: Adição do serviço SecretService para gerenciamento de segredos

---
+++ Services/Services/SecretService.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace KeyVaultPoC.Services
+{
+    public class SecretService
+    {
+        private readonly SecretClient _secretClient;
+
+        public SecretService(IConfiguration configuration)
+        {
+            string vaultUri = configuration["AzureKeyVault:VaultUri"];
+            _secretClient = new SecretClient(new Uri(vaultUri), new DefaultAzureCredential());
+        }
+
+        public async Task<string> GetSecretAsync(string secretName)
+        {
+            KeyVaultSecret secret = await _secretClient.GetSecretAsync(secretName);
+            return secret.Value;
+        }
+    }
+}
